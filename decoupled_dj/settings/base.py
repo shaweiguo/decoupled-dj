3b5303f407d5cf740b36fe8ab71372295862ecdb
shaweiguo <shaweiguo@yeah.net>
2022-11-26 19:18:45 +0800

    js 404

--- decoupled_dj/settings/base.py
+++ decoupled_dj/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 import environ
 from pathlib import Path
 
@@ -62,7 +63,7 @@ ROOT_URLCONF = "decoupled_dj.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
